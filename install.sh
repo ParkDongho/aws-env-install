2384423c24466f8bad4c12300e62d6e5afb81829
Dongho Park <parkdongho28@naver.com>
2023-02-27 14:54:36 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 mkdir /home/centos/src/scripts/
 curl https://s3.amazonaws.com/aws-fpga-developer-ami/1.5.0/Scripts/setup_gui.sh -o /home/centos/src/scripts/setup_gui.sh
-cd src/scripts/
+cd ~/src/scripts/
 chmod 700 setup_gui.sh
 ./setup_gui.sh
